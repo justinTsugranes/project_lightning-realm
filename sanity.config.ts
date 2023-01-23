4c404fd35a663bf5fcfeb089bc65ada0ba899daf
justintsugranes <justintsugranes@proton.me>
2023-01-22 20:53:59 -0500

    add iframe struc to sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,7 @@ import {schemaTypes} from './schemas'
 import {myTheme} from './theme'
 import StudioNavbar from './src/components/StudioNavbar'
 import StudioLogo from './src/components/StudioLogo'
+import {getDefaultDocumentNode} from './structure'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
@@ -16,7 +17,12 @@ export default defineConfig({
   projectId,
   dataset,
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode,
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
