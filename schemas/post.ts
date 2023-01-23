6562fd60badac56839fa2fbfd6abd4defd087e9d
justintsugranes <justintsugranes@proton.me>
2023-01-22 20:26:08 -0500

    add desc to post schema

--- schemas/post.ts
+++ schemas/post.ts
@@ -11,6 +11,12 @@ export default defineType({
       type: 'string',
     }),
     defineField({
+      name: 'description',
+      title: 'Description',
+      description: 'Enter a short snippet for the blog...',
+      type: 'string',
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
