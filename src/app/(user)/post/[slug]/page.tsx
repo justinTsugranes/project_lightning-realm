7fdca758acc5201eb7660daf34215ed184e68579
justintsugranes <justintsugranes@proton.me>
2023-01-23 17:04:03 -0500

    import rich text component

--- src/app/(user)/post/[slug]/page.tsx
+++ src/app/(user)/post/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import {groq} from 'next-sanity'
 import {PortableText} from '@portabletext/react'
+import {RichTextComponents} from '../../../../components/RichTextComponents'
 import urlFor from '../../../../../lib/urlFor'
 import {sanityClient} from '../../../../../lib/sanity.client'
 
