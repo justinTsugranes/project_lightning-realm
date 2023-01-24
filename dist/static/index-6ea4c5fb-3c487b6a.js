fea652aa67e436cc3675a8b6cfaaf474c0144e51
justintsugranes <justintsugranes@proton.me>
2023-01-24 10:36:49 -0500

    add favicon

---
+++ dist/static/index-6ea4c5fb-3c487b6a.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-66ec8894-16658762.js";import"./desk-eb7b3548-3b8198dc.js";import"./index-e2b0309d.js";export{m as DocumentPaneProvider,a as default};
