b6f033ddb0c71804c90023cd3b0674063ebfc60c
justintsugranes <justintsugranes@proton.me>
2023-02-12 06:44:51 -0500

    refactor urlfor into arrow func

--- lib/urlFor.ts
+++ lib/urlFor.ts
@@ -3,8 +3,6 @@ import imageUrlBuilder from '@sanity/image-url'
 
 const builder = imageUrlBuilder(sanityClient)
 
-function urlFor(source: any) {
-  return builder.image(source)
-}
+const urlFor = (source: any) => builder.image(source)
 
 export default urlFor
